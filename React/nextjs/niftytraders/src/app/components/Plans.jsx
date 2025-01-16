d664fcee054b206773eb78278a92b0b4acd1a0f7
69JINX <avimations39228@gmail.com>
2025-01-16 11:31:05 +0530

    responsive table niftytrader

--- React/nextjs/niftytraders/src/app/components/Plans.jsx
+++ React/nextjs/niftytraders/src/app/components/Plans.jsx
@@ -21,19 +21,19 @@ function Plans() {
             <div className={`text-black text-3xl py-5 ${poppins_head.className} text-center`}>
                 Pricing Plan
             </div>
-            <table className='p-10  text-black mx-10'>
+            <table className='p-10  text-black md:mx-10'>
                 <thead>
                     <tr>
                         <th className='p-5'>
                             <div className='flex'>
-                                <div className={`text-lg ${poppins_head.className}`}>
+                                <div className={`text-sm md:text-lg ${poppins_head.className}`}>
                                     Compare Plans
                                 </div>
-                                <div className={`border px-3 py-1 rounded-full text-sm ms-5 ${poppins_sub.className}`}>
+                                <div className={`border px-1 md:px-3 py-1 rounded-full text-[10px] ms-1 md:ms-5 ${poppins_sub.className}`}>
                                     60% Off
                                 </div>
                             </div>
-                            <div className='text-[#999999] py-4 text-sm font-light text-left leading-5'>
+                            <div className='text-[#999999] md:py-4 text-[10px] md:text-sm font-light text-left leading-5'>
                                 Choose your workspace plan according to your<br /> organization plan
                             </div>
                         </th>
@@ -44,10 +44,10 @@ function Plans() {
                             ].map((item, i) => (
                                 <th key={i} className='pb-4 text-center m-auto'>
                                     <div className='lg:flex items-end text-center py-2 lg:py-5 justify-center'>
-                                        <div className={`${poppins_head.className} leading-none text-2xl lg:text-4xl`}>{item.price}</div>
+                                        <div className={`${poppins_head.className} leading-none text-sm md:text-2xl lg:text-4xl`}>{item.price}</div>
                                         <div className={`${poppins_sub.className} ms-2 text-sm leading-6 text-[#999999]`}>{item.duration}</div>
                                     </div>
-                                    <div className='cursor-pointer py-3 mx-10 px-2 lg:px-16 rounded-md text-white bg-[#236AD4]'>
+                                    <div className='cursor-pointer text-[10px] md:text-lg py-3 mx-1 md:mx-10 px-2 lg:px-16 rounded-md text-white bg-[#236AD4]'>
                                         {item.button}
                                     </div>
                                 </th>
@@ -71,16 +71,16 @@ function Plans() {
                             { topic: 'Account Management', free: <SiTicktick className='m-auto' size={20} color='green' />, month: <SiTicktick className='m-auto' size={20} color='green' />, year: <SiTicktick className='m-auto' size={20} color='green' /> }
                         ].map((item, i) => (
                             <tr key={i}>
-                                <td className={`p-5 text-[12px] lg:text-sm  ${poppins_sub.className}`}>
+                                <td className={`p-1 md:p-5 text-[10px] lg:text-sm  ${poppins_sub.className}`}>
                                     {item.topic}
                                 </td>
-                                <td className={`text-center text-[12px] lg:text-sm ${poppins_sub.className}`}>
+                                <td className={`text-center p-0 text-[10px] lg:text-sm ${poppins_sub.className}`}>
                                     {item.free}
                                 </td>
-                                <td className={`text-center text-[12px] lg:text-sm ${poppins_sub.className}`}>
+                                <td className={`text-center text-[10px] lg:text-sm ${poppins_sub.className}`}>
                                     {item.month}
                                 </td>
-                                <td className={`text-center text-[12px] lg:text-sm ${poppins_sub.className}`}>
+                                <td className={`text-center text-[10px] lg:text-sm ${poppins_sub.className}`}>
                                     {item.year}
                                 </td>
                             </tr>
