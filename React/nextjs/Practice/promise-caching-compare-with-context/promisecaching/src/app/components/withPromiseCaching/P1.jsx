062082f63a2038c1f6336c6983d1707ce6e301f3
avinashnowawave <avinash.nowawave@gmail.com>
2025-04-29 10:50:40 +0530

    Added promise-caching project properly as folder

---
+++ React/nextjs/Practice/promise-caching-compare-with-context/promisecaching/src/app/components/withPromiseCaching/P1.jsx
@@ -0,0 +1,30 @@
+'use client'
+import { getData } from '@/api/api';
+import { useEffect, useState } from 'react';
+
+function P1() {
+
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    const data = await getData('https://jsonplaceholder.typicode.com/users/1');
+    console.log(data);
+    setData(data);
+  }
+  useEffect(() => {
+    fetchData();
+    return () => setData(null)
+  }, [])
+
+  return (
+    <div>{
+      data ?
+        <pre>
+          {JSON.stringify(data.company, null, 2)}
+        </pre>
+        : <>Loading...</>
+    }</div>
+  )
+}
+
+export default P1
---
+++ React/nextjs/Practice/promise-caching-compare-with-context/promisecaching/src/app/components/withPromiseCaching/P2.jsx
@@ -0,0 +1,28 @@
+'use client'
+import { getData } from '@/api/api';
+import React, { useEffect, useState } from 'react'
+
+function P2() {
+
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    const data = await getData('https://jsonplaceholder.typicode.com/users/1');
+    setData(data);
+  }
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  return (
+    <div>{
+      data ?
+        <pre>
+          {JSON.stringify(data.company, null, 2)}
+        </pre>
+        : <>Loading...</>
+    }</div>
+  )
+}
+
+export default P2
