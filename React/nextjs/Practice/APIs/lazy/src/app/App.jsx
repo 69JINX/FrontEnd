d6793dbdd5e7edea42d930c7fd8fe712b69ce8bd
69JINX <avimations39228@gmail.com>
2025-05-01 22:10:11 +0530

    lazy pngj

--- React/nextjs/Practice/APIs/lazy/src/app/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function App() {
-  return (
-    <div className='border p-2'>Lazy Loaded Component</div>
-  )
-}
-
-export default App
--- React/nextjs/Practice/APIs/lazy/src/app/App1.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function App1() {
-  return (
-    <div className='border p-2'>Normal Component</div>
-  )
-}
-
-export default App1
