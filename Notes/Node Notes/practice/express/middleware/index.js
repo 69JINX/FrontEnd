f751873567868ba3b744e7b29c531179654f00b7
avinashnowawave <avinash.nowawave@gmail.com>
2025-07-29 19:07:39 +0530

    express middlewares

--- Notes/Node Notes/practice/express/middleware/index.js
+++ Notes/Node Notes/practice/express/middleware/index.js
@@ -3,12 +3,32 @@ const app = express();
 
 
 const middleware = (req,res,next)=>{
+ res.data = {
+    data1:'data1'   
+ }
+ res.locals.user = 'Avinash';
  next();
 }
+app.use(middleware);
+app.get('/test',(req,res)=>{
+   console.log(res.headersSent);
+   res.set('Content-Type', 'application/json');
+   console.log(res.headersSent);
+   res.send({ message: 'Hello' });
+   console.log(res.headersSent);
+});
 
+app.get('/',(req,res)=>{
+  res.data = {
+    ...res.data,
+    data2:'data2'
+  }
+  res.json(res.locals);
+});
 
-app.get('/',middleware,(req,res)=>{
-  res.send('Home Page');
+app.use((err, req, res, next) => {
+  console.error('Error >>>',err.stack);
+  res.status(500).send('Something broke!');
 });
 
 app.listen(4000,()=>{
